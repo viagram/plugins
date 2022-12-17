ee452e3dfebd62c97dd19e896d48d43642b36f6b
viagram <viagram@users.noreply.github.com>
2022-12-17 21:06:28 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -59,13 +59,13 @@ download_brook() {
 }
 
 download_hysteria() {
-  VERSION="v1.3.1-1"
+  VERSION="v1.3.2-1"
   mkdir_libs "app_hysteria/libs"
 
-  dl_and_chmod arm64-v8a/libhysteria.so "https://github.com/MatsuriDayo/hysteria/releases/download/$VERSION/hysteria-linux-arm64"
-  dl_and_chmod armeabi-v7a/libhysteria.so "https://github.com/MatsuriDayo/hysteria/releases/download/$VERSION/hysteria-linux-arm"
-  dl_and_chmod x86/libhysteria.so "https://github.com/MatsuriDayo/hysteria/releases/download/$VERSION/hysteria-linux-386"
-  dl_and_chmod x86_64/libhysteria.so "https://github.com/MatsuriDayo/hysteria/releases/download/$VERSION/hysteria-linux-amd64"
+  dl_and_chmod arm64-v8a/libhysteria.so "https://github.com/viagram/hysteria/releases/download/$VERSION/hysteria-linux-arm64"
+  dl_and_chmod armeabi-v7a/libhysteria.so "https://github.com/viagram/hysteria/releases/download/$VERSION/hysteria-linux-arm"
+  dl_and_chmod x86/libhysteria.so "https://github.com/viagram/hysteria/releases/download/$VERSION/hysteria-linux-386"
+  dl_and_chmod x86_64/libhysteria.so "https://github.com/viagram/hysteria/releases/download/$VERSION/hysteria-linux-amd64"
 }
 
 download_"$1"
